c1d6968de4bb9e576faa9e800704790b7dacaffb
Kingsley Lam-Li <infinityoreos@Kingsleys-burning-pot-of-soup.local>
2026-07-19 14:34:55 +1000

    Began using Imgcodecs module - the module processes images from the computer - comments are made in the code

--- camera-project/src/main/java/com/infinityoreos/App.java
+++ camera-project/src/main/java/com/infinityoreos/App.java
@@ -4,6 +4,7 @@ import nu.pattern.OpenCV;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
 
 public class App {
 
@@ -15,31 +16,22 @@ public class App {
         System.out.println("OpenCV version: " + Core.VERSION);
         System.out.println("");
 
-        // Creating a Mat of 640x480 using CvType.CV_8UC3
-        // note that for a image of width x height, Mat uses Rows first then Columns
-        // so we gotta inverse that thang
-        // so it gotta be ... new Mat(480, 640, ...) not ... new Mat(640, 480, ...)
-        Mat image = new Mat(480, 640, CvType.CV_8UC3);
+        // assign the mat image the mat of the image file Lena.jpg.
+        String imageName = "Lena.png";
+        Mat image = Imgcodecs.imread(imageName);
+        System.out.println("The file name is " + imageName);
 
         // printing the number of rows, columns, channels, size, whether it's empty
         System.out.println(image.rows());
         System.out.println(image.cols());
         System.out.println(image.channels());
         System.out.println(image.size());
-        System.out.println(image.empty());
-        // assigning the pixel at (0,0) to be Blue (remember that coordinates are
-        // assigned as (row, column, values) not (x, y, values). This means that (100,
-        // 200, colour) is approximately x = 200, y = 100, colour = colour)
-        image.put(0, 0, new double[] { 255, 0, 0 });
 
-        // Getting the raw value of the pixel at (0,0) (it looks like a mess).
-        // It looks like a mess because Java is printing the internal representation of
-        // an array when we pass it into println().
-
-        // Assigning the array as an array of doubles in pixel is the correct approach.
-        // We can also print the array cleanly with Arrays.toString(); from
-        // import.java.util.Arrays;
-        System.out.println(image.get(0, 0));
+        if (image.empty()) {
+            System.out.println("The image could not be found!");
+            image.release();
+            return;
+        }
 
         // reading and printing out the BGR values of that same pixel at (0,0)
         double[] pixel = image.get(0, 0);
@@ -47,9 +39,19 @@ public class App {
         System.out.println(pixel[1]);
         System.out.println(pixel[2]);
 
+        // now, reassign the value of the pixel at that point to be blue.
+
+        image.put(0, 0, new double[] { 255, 0, 0 });
+
+        // I want to begin seeing the images. The image will be saved.
+
+        // imwrite is a boolean. Capture it to allow it to be used.
+        boolean ImageSaved = Imgcodecs.imwrite("output.png", image);
+
+        System.out.println("Image saved: " + ImageSaved);
+
         // (post feedback) it is good to release native memory (Mat) when finished.
         // this is important in bigger projects, eg. continuously reading a camera feed.
-
         image.release();
     }
 }
