31291d4c5554e29217687312d162cc28f248864d
Kingsley Lam-Li <infinityoreos@Kingsleys-burning-pot-of-soup.local>
2026-07-23 00:37:22 +1000

    Added live camera functionality - default camera of OS used - pleased with first iteration

--- camera-project/src/main/java/com/infinityoreos/App.java
+++ camera-project/src/main/java/com/infinityoreos/App.java
@@ -3,12 +3,13 @@ package com.infinityoreos;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.Rect;
+import org.opencv.core.Size;
 import org.opencv.core.Scalar;
 import org.opencv.highgui.HighGui;
 import org.opencv.core.MatOfRect;
-import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
+import org.opencv.videoio.VideoCapture;
 
 import nu.pattern.OpenCV;
 
@@ -22,53 +23,100 @@ public class App {
         System.out.println("OpenCV version: " + Core.VERSION);
         System.out.println("");
 
-        // prepare the cascade, images, and MatOfRect for facial detection
-        String cascadeName = "src/main/resources/haarcascade_frontalface_default.xml";
-        String imageName = "face1.jpeg";
-
-        Mat originalImage = Imgcodecs.imread("src/main/resources/" + imageName);
-        if (originalImage.empty()) {
-            System.out.println("Image failed to load!");
-            originalImage.release();
-            return;
-        }
-
-        Mat imageRectAroundFaces = originalImage.clone();
-        Mat imageGrayScale = new Mat();
+        // prepare the cascade, images, and MatOfRect for facial detection on a LIVE
+        // CAMERA.
+        Mat frame = new Mat();
+        Mat rectangleFrame = new Mat();
+        Mat greyFrame = new Mat();
 
         MatOfRect faces = new MatOfRect();
+        MatOfRect sides = new MatOfRect();
 
-        CascadeClassifier classifier = new CascadeClassifier(cascadeName);
+        Rect[] facesArray;
+        Rect[] sidesArray;
 
-        if (classifier.empty()) {
-            System.out.println("Classifier failed to load!");
+        String faceCascadePath = "src/main/resources/haarcascade_frontalface_default.xml";
+        String sideCascadePath = "src/main/resources/haarcascade_profileface.xml";
+
+        // check they have correctly loaded
+        CascadeClassifier faceClassifier = new CascadeClassifier(faceCascadePath);
+        if (faceClassifier.empty()) {
+            System.out.println("Could not load front face front classifier!");
             return;
         }
 
-        // make the grayscale copy
-        Imgproc.cvtColor(originalImage, imageGrayScale, Imgproc.COLOR_BGR2GRAY);
+        CascadeClassifier sideClassifier = new CascadeClassifier(sideCascadePath);
 
-        // detect the faces
-        classifier.detectMultiScale(imageGrayScale, faces);
-        // convert our Mat of faces to a Rect[] to iterate over
-        Rect[] facesArray = faces.toArray();
-
-        System.out.println("Number of faces found: " + facesArray.length);
-
-        for (Rect face : facesArray) {
-            System.out.println(face);
-            Imgproc.rectangle(
-                    imageRectAroundFaces, face, new Scalar(0, 255, 0, 2)); // the '2' is thickness
+        if (sideClassifier.empty()) {
+            System.out.println("Could not load side profile cascade!");
+            return;
         }
 
-        // HighGui.imshow("Grayscale", imageGrayScale);
-        HighGui.imshow("Original with Rectangles", imageRectAroundFaces);
-        HighGui.waitKey(0);
+        VideoCapture camera = new VideoCapture(0);
 
-        originalImage.release();
-        imageRectAroundFaces.release();
-        imageGrayScale.release();
+        if (!camera.isOpened()) {
+            System.out.println("Frame failed to open!");
+            return;
+        }
+        ;
+
+        while (camera.read(frame)) {
+            // The camera is actively reading frames.
+            Imgproc.cvtColor(
+                    frame,
+                    greyFrame,
+                    Imgproc.COLOR_BGR2GRAY);
+            rectangleFrame = frame.clone();
+
+            // feed classifier the grey frames
+            faceClassifier.detectMultiScale(
+                    greyFrame,
+                    faces,
+                    1.1,
+                    5,
+                    0,
+                    new Size(30, 30));
+
+            sideClassifier.detectMultiScale(
+                    greyFrame,
+                    sides,
+                    1.1,
+                    5,
+                    0,
+                    new Size(30, 30));
+
+            facesArray = faces.toArray();
+            sidesArray = sides.toArray();
+
+            // Add the rectangles for front of face and side of face
+            for (Rect face : facesArray) {
+                Imgproc.rectangle(rectangleFrame, face, new Scalar(0, 0, 255, 10));
+            }
+
+            for (Rect side : sidesArray) {
+                Imgproc.rectangle(rectangleFrame, side, new Scalar(0, 255, 0, 10));
+            }
+
+            // Display the image
+            HighGui.imshow("Face tracker", rectangleFrame);
+            int key = HighGui.waitKey(10);
+
+            if (key == 'Q') {
+                System.out.println("Quitting camera.");
+                break;
+            }
+
+        }
+        // Cleaning up
+        System.out.println("Cleaning up.");
+
+        frame.release();
+        rectangleFrame.release();
+        greyFrame.release();
         faces.release();
+        sides.release();
+        camera.release();
+
         HighGui.destroyAllWindows();
         System.exit(0);
 
